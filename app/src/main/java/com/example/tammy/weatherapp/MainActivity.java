06c86d2d3848f386df146afbae9f00b855f4b0f6
ctam91 <ctam91@gmail.com>
2017-10-06 18:02:51 -0700

    add hardcoded arraylist to test

--- app/src/main/java/com/example/tammy/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/tammy/weatherapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tammy.weatherapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Weather> weathers = new ArrayList<Weather>();
+        weathers.add(new Weather("Lynnwood", 72.0));
+        weathers.add(new Weather("Seattle", 64.3));
     }
 }
