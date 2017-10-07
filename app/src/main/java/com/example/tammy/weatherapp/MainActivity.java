5e0bcb905059996993086c3d4297d7c3380542f6
ctam91 <ctam91@gmail.com>
2017-10-06 18:13:25 -0700

    update WeatherAdapter

--- app/src/main/java/com/example/tammy/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/tammy/weatherapp/MainActivity.java
@@ -2,18 +2,26 @@ package com.example.tammy.weatherapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 import java.util.ArrayList;
+
+import static com.example.tammy.weatherapp.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
 
         ArrayList<Weather> weathers = new ArrayList<Weather>();
         weathers.add(new Weather("Lynnwood", 72.0));
         weathers.add(new Weather("Seattle", 64.3));
+
+        WeatherAdapter adapter = new WeatherAdapter(this, weathers);
+        ListView weatherListView = (ListView) findViewById(R.id.list);
+
+        weatherListView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/tammy/weatherapp/WeatherAdapter.java
+++ app/src/main/java/com/example/tammy/weatherapp/WeatherAdapter.java
@@ -2,7 +2,11 @@ package com.example.tammy.weatherapp;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -14,5 +18,26 @@ public class WeatherAdapter extends ArrayAdapter<Weather> {
 
     public WeatherAdapter(@NonNull Context context, @NonNull List<Weather> objects) {
         super(context, 0, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.weather_list_item, parent, false);
+        }
+        Weather currentWeather= getItem(position);
+
+        TextView city = (TextView) listItemView.findViewById(R.id.city);
+        String currentCity = currentWeather.getCity();
+        city.setText(currentCity);
+
+
+        TextView temp = (TextView) listItemView.findViewById(R.id.temp);
+        Double currentTemp = currentWeather.getTemp();
+        temp.setText(String.valueOf(currentTemp));
+
+        return listItemView;
     }
 }
